1d098ca45d417ae10345ac658b68404c879d65c0
Tessa Lau <tlau@tlau.org>
2014-01-09 10:17:59 -0300

    Added REST API for retrieving data in JSON format

--- server/router.js
+++ server/router.js
@@ -15,3 +15,20 @@ Meteor.Router.add('/send_notification', function() {
   }
   return [200, 'OK'];
 });
+
+Meteor.Router.add('/data', function() {
+  console.log('Returning samples for query:', this.request.query);
+  var result = {};
+  if (this.request.query && this.request.query.phone) {
+    var canonical = this.request.query.phone;
+    var orig = this.request.query.phone;
+    if (canonical[0] != "+") {
+      canonical = "+1" + canonical.replace(/-/g, "");
+    }
+    result.samples = Samples.find({$or: [{From: canonical}, {From: orig}]}).fetch();
+    result.prompts = Prompts.find({$or: [{to: canonical},
+      {to: orig}]}).fetch();
+  }
+  return [200, JSON.stringify(result)];
+});
+
